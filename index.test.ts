3f93a61d1e1337fba1a5db71f3508bc8948376e7
Zehao Zhang <zehao93@outlook.com>
2024-06-11 09:55:46 +0800

    fix: type testing

--- index.test.ts
+++ index.test.ts
@@ -2,39 +2,69 @@ import { TEventEmitter } from '.'
 
 const emitter = new TEventEmitter()
 
-const expectType = <Type>(_: Type): void => void 0;
+export type Expect<T extends true> = T
+export type Equal<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false
+export type NotEqual<X, Y> = true extends Equal<X, Y> ? false : true
 
 const e1 = emitter.createKey<[string, number, { foo: string }]>('e1')
 
 emitter.on(e1, (a1, a2, a3) => {
-  expectType<string>(a1)
-  expectType<number>(a2)
-  expectType<{ foo: string }>(a3)
-  console.log(a1, a2, a3)
+  type cases = [
+    Expect<NotEqual<typeof a1, any>>,
+    Expect<NotEqual<typeof a2, any>>,
+    Expect<NotEqual<typeof a3, any>>,
+    Expect<Equal<string, typeof a1>>,
+    Expect<Equal<number, typeof a2>>,
+    Expect<Equal<{ foo: string }, typeof a3>>,
+  ]
 })
 
 emitter.once(e1, (a1, a2, a3) => {
-  expectType<string>(a1)
-  expectType<number>(a2)
-  expectType<{ foo: string }>(a3)
+  type cases = [
+    Expect<NotEqual<typeof a1, any>>,
+    Expect<NotEqual<typeof a2, any>>,
+    Expect<NotEqual<typeof a3, any>>,
+    Expect<Equal<string, typeof a1>>,
+    Expect<Equal<number, typeof a2>>,
+    Expect<Equal<{ foo: string }, typeof a3>>,
+  ]
 })
 
 emitter.addListener(e1, (a1, a2, a3) => {
-  expectType<string>(a1)
-  expectType<number>(a2)
-  expectType<{ foo: string }>(a3)
+  type cases = [
+    Expect<NotEqual<typeof a1, any>>,
+    Expect<NotEqual<typeof a2, any>>,
+    Expect<NotEqual<typeof a3, any>>,
+    Expect<Equal<string, typeof a1>>,
+    Expect<Equal<number, typeof a2>>,
+    Expect<Equal<{ foo: string }, typeof a3>>,
+  ]
 })
 
 emitter.removeListener(e1, (a1, a2, a3) => {
-  expectType<string>(a1)
-  expectType<number>(a2)
-  expectType<{ foo: string }>(a3)
+  type cases = [
+    Expect<NotEqual<typeof a1, any>>,
+    Expect<NotEqual<typeof a2, any>>,
+    Expect<NotEqual<typeof a3, any>>,
+    Expect<Equal<string, typeof a1>>,
+    Expect<Equal<number, typeof a2>>,
+    Expect<Equal<{ foo: string }, typeof a3>>,
+  ]
 })
 
 emitter.off(e1, (a1, a2, a3) => {
-  expectType<string>(a1)
-  expectType<number>(a2)
-  expectType<{ foo: string }>(a3)
+  type cases = [
+    Expect<NotEqual<typeof a1, any>>,
+    Expect<NotEqual<typeof a2, any>>,
+    Expect<NotEqual<typeof a3, any>>,
+    Expect<Equal<string, typeof a1>>,
+    Expect<Equal<number, typeof a2>>,
+    Expect<Equal<{ foo: string }, typeof a3>>,
+  ]
 })
 
-emitter.emit(e1, 'string', 42, { foo: 'bar' })
+type ExpectArgTypes = [string, number]
+type ArgTypes = typeof emitter.emit<ExpectArgTypes> extends (eventName: any, ...args: infer U) => boolean ? U : never
+type ArgTypeCase = Expect<Equal<ExpectArgTypes, ArgTypes>>
